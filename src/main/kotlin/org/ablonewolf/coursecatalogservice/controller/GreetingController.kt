c8005b4cd5a41daca1d6164d5bdbf0969c58c0d6
Arka Bhuiyan <belmont66@duck.com>
2025-08-24 19:48:30 +0600

    injected the Greetings Service through the constructor of the GreetingController, added a log object to log appropriate statements, also got rid of the redundant parentheses.

--- src/main/kotlin/org/ablonewolf/coursecatalogservice/controller/GreetingController.kt
+++ src/main/kotlin/org/ablonewolf/coursecatalogservice/controller/GreetingController.kt
@@ -1,5 +1,7 @@
 package org.ablonewolf.coursecatalogservice.controller
 
+import org.ablonewolf.coursecatalogservice.service.GreetingsService
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -7,10 +9,16 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/greetings")
-class GreetingController {
+class GreetingController(
+    private val greetingsService: GreetingsService
+) {
+    companion object {
+        private val log = LoggerFactory.getLogger(GreetingController::class.java)
+    }
 
-    @GetMapping()
+    @GetMapping
     fun retrieveGreeting(@RequestParam("name") name: String): String {
-        return "Hello there ${name}, welcome to the Course Catalog Service!"
+        log.info("Greeting API called with name: {}", name)
+        return greetingsService.retrieveGreeting(name)
     }
 }
