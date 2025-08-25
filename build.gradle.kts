18a3702345a710d07756e8e2d0022d6b9e4c6353
Arka Bhuiyan <belmont66@duck.com>
2025-08-26 03:18:30 +0600

    added the dependency for mockito of Kotlin language.

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,10 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webflux")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+
+    // https://mvnrepository.com/artifact/org.mockito.kotlin/mockito-kotlin
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
