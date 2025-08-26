edc03555e5923220b3ee5dbc70662deb199c4dcb
Arka Bhuiyan <belmont66@duck.com>
2025-08-26 20:15:44 +0600

    added the repository layer method to fetch Course list as a page object.

--- src/main/kotlin/org/ablonewolf/coursecatalogservice/repository/CourseRepository.kt
+++ src/main/kotlin/org/ablonewolf/coursecatalogservice/repository/CourseRepository.kt
@@ -1,9 +1,31 @@
 package org.ablonewolf.coursecatalogservice.repository
 
+import org.ablonewolf.coursecatalogservice.model.dto.request.CourseSearchDTO
+import org.ablonewolf.coursecatalogservice.model.dto.response.CourseResponseDTO
 import org.ablonewolf.coursecatalogservice.model.entity.Course
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
 interface CourseRepository : JpaRepository<Course, Int> {
+
+    @Query(
+        value = """
+    SELECT 
+        course.id AS id, 
+        course.name AS name, 
+        course.category AS category, 
+        course.description AS description 
+    FROM Course course
+    WHERE
+        (:#{#searchDTO.name} IS NULL OR course.name LIKE CONCAT('%', :#{#searchDTO.name}, '%'))
+        AND (:#{#searchDTO.category} IS NULL OR course.category LIKE CONCAT('%', :#{#searchDTO.category}, '%'))"""
+    )
+    fun getAllCourses(
+        pageable: Pageable, @Param("searchDTO") courseSearchDTO: CourseSearchDTO
+    ): Page<CourseResponseDTO>
 }
