36c17b7602cd464e9f32ddb3a7a61ba13d22cd9f
Arka Bhuiyan <belmont66@duck.com>
2025-08-27 19:18:00 +0600

    completed the controller layer code for the API of deleting a course by its Id.

--- src/main/kotlin/org/ablonewolf/coursecatalogservice/controller/CourseController.kt
+++ src/main/kotlin/org/ablonewolf/coursecatalogservice/controller/CourseController.kt
@@ -10,6 +10,7 @@ import org.ablonewolf.coursecatalogservice.service.CourseService
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
@@ -61,5 +62,13 @@ class CourseController(
 		log.info("Creating multiple new courses, count: ${courseCreateDTOs.size}")
 		courseService.createMultipleCourses(courseCreateDTOs)
 		return ResponseEntity.status(HttpStatus.CREATED.value()).build<Unit>()
+	}
+
+	@DeleteMapping("/{id}")
+	@Operation(summary = "Delete a course by its id")
+	fun deleteCourse(@PathVariable id: Int): ResponseEntity<Unit> {
+		log.info("Deleting course with id: $id")
+		courseService.deleteCourse(id)
+		return ResponseEntity.status(HttpStatus.NO_CONTENT.value()).build<Unit>()
 	}
 }
