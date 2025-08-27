41f06437b4fbe74f05c3bb92495f86cbe187a1a6
Arka Bhuiyan <belmont66@duck.com>
2025-08-27 08:15:35 +0600

    created two new service layer methods - one for updating existing course, and another for saving multiple courses. Completed the concrete implementation for both of the methods.

--- src/main/kotlin/org/ablonewolf/coursecatalogservice/service/CourseService.kt
+++ src/main/kotlin/org/ablonewolf/coursecatalogservice/service/CourseService.kt
@@ -2,12 +2,17 @@ package org.ablonewolf.coursecatalogservice.service
 
 import org.ablonewolf.coursecatalogservice.model.dto.request.CourseCreateDTO
 import org.ablonewolf.coursecatalogservice.model.dto.request.CourseSearchDTO
+import org.ablonewolf.coursecatalogservice.model.dto.request.CourseUpdateDTO
 import org.ablonewolf.coursecatalogservice.model.dto.response.CourseResponseDTO
 import org.ablonewolf.coursecatalogservice.model.dto.response.PageData
 
 interface CourseService {
 
-    fun createNewCourse(courseCreateDTO: CourseCreateDTO) : CourseResponseDTO
+	fun createNewCourse(courseCreateDTO: CourseCreateDTO): CourseResponseDTO
 
-    fun getAllCourses(courseSearchDTO: CourseSearchDTO): PageData<CourseResponseDTO>
+	fun getAllCourses(courseSearchDTO: CourseSearchDTO): PageData<CourseResponseDTO>
+
+	fun updateCourse(id: Int, courseUpdateDTO: CourseUpdateDTO): CourseResponseDTO
+
+	fun createMultipleCourses(courseCreateDTOs: List<CourseCreateDTO>)
 }
--- src/main/kotlin/org/ablonewolf/coursecatalogservice/service/impl/CourseServiceImpl.kt
+++ src/main/kotlin/org/ablonewolf/coursecatalogservice/service/impl/CourseServiceImpl.kt
@@ -2,6 +2,7 @@ package org.ablonewolf.coursecatalogservice.service.impl
 
 import org.ablonewolf.coursecatalogservice.model.dto.request.CourseCreateDTO
 import org.ablonewolf.coursecatalogservice.model.dto.request.CourseSearchDTO
+import org.ablonewolf.coursecatalogservice.model.dto.request.CourseUpdateDTO
 import org.ablonewolf.coursecatalogservice.model.dto.response.CourseResponseDTO
 import org.ablonewolf.coursecatalogservice.model.dto.response.PageData
 import org.ablonewolf.coursecatalogservice.model.entity.Course
@@ -14,40 +15,82 @@ import org.springframework.stereotype.Service
 @Service
 class CourseServiceImpl(private val courseRepository: CourseRepository) : CourseService {
 
-    companion object {
-        private val log = LoggerFactory.getLogger(CourseServiceImpl::class.java)
-    }
+	companion object {
+		private val log = LoggerFactory.getLogger(CourseServiceImpl::class.java)
+	}
 
-    override fun createNewCourse(courseCreateDTO: CourseCreateDTO): CourseResponseDTO {
-        val course = courseCreateDTO.let {
-            Course(
-                name = it.name,
-                category = it.category,
-                description = it.description
-            )
-        }
+	override fun createNewCourse(courseCreateDTO: CourseCreateDTO): CourseResponseDTO {
+		val course = courseCreateDTO.let {
+			Course(
+				name = it.name,
+				category = it.category,
+				description = it.description
+			)
+		}
 
-        courseRepository.save(course)
-        log.info("Created new course with name ${course.name}, its id is: ${course.id}")
+		courseRepository.save(course)
+		log.info("Created new course with name ${course.name}, its id is: ${course.id}")
 
-        return course.let {
-            object : CourseResponseDTO {
-                override val id: Int? = it.id
-                override val name: String? = it.name
-                override val category: String? = it.category
-                override val description: String? = it.description
-            }
-        }
-    }
+		return course.let {
+			object : CourseResponseDTO {
+				override val id: Int? = it.id
+				override val name: String? = it.name
+				override val category: String? = it.category
+				override val description: String? = it.description
+			}
+		}
+	}
 
-    override fun getAllCourses(courseSearchDTO: CourseSearchDTO): PageData<CourseResponseDTO> {
-        val pageable = PageRequest.of(courseSearchDTO.pageNumber - 1, courseSearchDTO.pageSize)
-        val coursePage = courseRepository.getAllCourses(pageable, courseSearchDTO)
-        return PageData(
-            contents = coursePage.content,
-            currentPage = coursePage.number + 1,
-            totalElements = coursePage.totalElements,
-            totalPages = coursePage.totalPages
-        )
-    }
+	override fun getAllCourses(courseSearchDTO: CourseSearchDTO): PageData<CourseResponseDTO> {
+		val pageable = PageRequest.of(courseSearchDTO.pageNumber - 1, courseSearchDTO.pageSize)
+		val coursePage = courseRepository.getAllCourses(pageable, courseSearchDTO)
+		return PageData(
+			contents = coursePage.content,
+			currentPage = coursePage.number + 1,
+			totalElements = coursePage.totalElements,
+			totalPages = coursePage.totalPages
+		)
+	}
+
+	override fun updateCourse(
+		id: Int,
+		courseUpdateDTO: CourseUpdateDTO
+	): CourseResponseDTO {
+		val existingCourse = courseRepository.findById(id)
+			.orElseThrow { IllegalArgumentException("Course with id $id not found") }
+
+		existingCourse?.let { course ->
+			course.name = courseUpdateDTO.name ?: course.name
+			course.category = courseUpdateDTO.category ?: course.category
+		}
+		courseRepository.save(existingCourse)
+		log.info("Updated course with id: $id")
+
+		return existingCourse.let { course ->
+			object : CourseResponseDTO {
+				override val id: Int? = course.id
+				override val name: String? = course.name
+				override val category: String? = course.category
+				override val description: String? = course.description
+			}
+		}
+	}
+
+	override fun createMultipleCourses(
+		courseCreateDTOs: List<CourseCreateDTO>) {
+		val createdCourses = mutableListOf<Course>()
+
+		courseCreateDTOs.forEach {
+			val course = Course(
+				name = it.name,
+				category = it.category,
+				description = it.description
+			)
+			createdCourses.add(course)
+		}
+
+		courseRepository.saveAll(createdCourses)
+		log.info("Created ${createdCourses.size} new courses")
+	}
 }
+
