8ef10567bafc50fea345f7c1ce749daf1143cbd6
Arka Bhuiyan <belmont66@duck.com>
2025-08-27 07:43:45 +0600

    changed the type of all fields in the Course entity - from "val" to "var" since all fields are subject to modification since they are part of a JPA entity.

--- src/main/kotlin/org/ablonewolf/coursecatalogservice/model/entity/Course.kt
+++ src/main/kotlin/org/ablonewolf/coursecatalogservice/model/entity/Course.kt
@@ -10,16 +10,16 @@ import jakarta.persistence.Table
 @Entity
 @Table(name = "courses")
 class Course(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Int? = null,
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	var id: Int? = null,
 
-    @Column(name = "name", nullable = false, length = 64)
-    val name: String,
+	@Column(name = "name", nullable = false, length = 64)
+	var name: String,
 
-    @Column(name = "description", nullable = false, columnDefinition = "text")
-    val description: String,
+	@Column(name = "description", nullable = false, columnDefinition = "text")
+	var description: String,
 
-    @Column(name = "category", nullable = false, length = 32)
-    val category: String
+	@Column(name = "category", nullable = false, length = 32)
+	var category: String
 ) : BaseEntity()
