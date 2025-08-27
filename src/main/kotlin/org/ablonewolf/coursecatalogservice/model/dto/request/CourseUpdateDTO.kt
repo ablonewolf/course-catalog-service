4ff5556a6eda531df28a5a9bc9ae3f17bc0085c3
Arka Bhuiyan <belmont66@duck.com>
2025-08-27 08:14:24 +0600

    created a new DTO for updating existing courses.

---
+++ src/main/kotlin/org/ablonewolf/coursecatalogservice/model/dto/request/CourseUpdateDTO.kt
@@ -0,0 +1,9 @@
+package org.ablonewolf.coursecatalogservice.model.dto.request
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+
+data class CourseUpdateDTO(
+    @JsonIgnore
+    val name: String?,
+    val category: String?
+)
