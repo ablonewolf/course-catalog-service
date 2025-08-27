71a24d3c7837592598fad425ee6050cbcca2225d
Arka Bhuiyan <belmont66@duck.com>
2025-08-27 08:18:14 +0600

    completed the two pending APIs - one for updating an existing course, and the other for saving multiple courses in batch.

--- src/main/kotlin/org/ablonewolf/coursecatalogservice/controller/CourseController.kt
+++ src/main/kotlin/org/ablonewolf/coursecatalogservice/controller/CourseController.kt
@@ -2,13 +2,16 @@ package org.ablonewolf.coursecatalogservice.controller
 
 import org.ablonewolf.coursecatalogservice.model.dto.request.CourseCreateDTO
 import org.ablonewolf.coursecatalogservice.model.dto.request.CourseSearchDTO
+import org.ablonewolf.coursecatalogservice.model.dto.request.CourseUpdateDTO
 import org.ablonewolf.coursecatalogservice.model.dto.response.CourseResponseDTO
 import org.ablonewolf.coursecatalogservice.model.dto.response.PageData
 import org.ablonewolf.coursecatalogservice.service.CourseService
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -16,23 +19,42 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/courses")
 class CourseController(
-    private val courseService: CourseService
+	private val courseService: CourseService
 ) {
-    companion object {
-        private val log = LoggerFactory.getLogger(CourseController::class.java)
-    }
+	companion object {
+		private val log = LoggerFactory.getLogger(CourseController::class.java)
+	}
 
-    @PostMapping
-    fun createCourse(@RequestBody courseCreateDTO: CourseCreateDTO): ResponseEntity<CourseResponseDTO> {
-        log.info("Creating new course with name ${courseCreateDTO.name}")
-        val createdCourse = courseService.createNewCourse(courseCreateDTO)
-        return ResponseEntity.status(HttpStatus.CREATED.value()).body(createdCourse)
-    }
+	@PostMapping
+	fun createCourse(@RequestBody courseCreateDTO: CourseCreateDTO): ResponseEntity<CourseResponseDTO> {
+		log.info("Creating new course with name ${courseCreateDTO.name}")
+		val createdCourse = courseService.createNewCourse(courseCreateDTO)
+		return ResponseEntity.status(HttpStatus.CREATED.value()).body(createdCourse)
+	}
 
-    @PostMapping("/get-all")
-    fun getAllCourses(@RequestBody courseSearchDTO: CourseSearchDTO): ResponseEntity<PageData<CourseResponseDTO>> {
-        log.info("Fetching all course existing courses")
-        val courses = courseService.getAllCourses(courseSearchDTO)
-        return ResponseEntity.status(HttpStatus.OK.value()).body(courses)
-    }
+	@PostMapping("/get-all")
+	fun getAllCourses(@RequestBody courseSearchDTO: CourseSearchDTO): ResponseEntity<PageData<CourseResponseDTO>> {
+		log.info("Fetching all course existing courses")
+		val courses = courseService.getAllCourses(courseSearchDTO)
+		return ResponseEntity.status(HttpStatus.OK.value()).body(courses)
+	}
+
+	@PutMapping("/{id}")
+	fun updateCourse(
+		@PathVariable id: Int,
+		@RequestBody courseUpdateDTO: CourseUpdateDTO
+	): ResponseEntity<CourseResponseDTO> {
+		log.info("Updating course with id: $id")
+		val updatedCourse = courseService.updateCourse(id, courseUpdateDTO)
+		return ResponseEntity.ok()
+			.body(updatedCourse)
+	}
+
+	@PostMapping("/batch")
+	fun createMultipleCourses(
+		@RequestBody courseCreateDTOs: List<CourseCreateDTO>): ResponseEntity<Unit> {
+		log.info("Creating multiple new courses, count: ${courseCreateDTOs.size}")
+		courseService.createMultipleCourses(courseCreateDTOs)
+		return ResponseEntity.status(HttpStatus.CREATED.value()).build<Unit>()
+	}
 }
