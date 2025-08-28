e1f8e8dfa5d060089c4196d9d064fcdb3db867c0
Arka Bhuiyan <belmont66@duck.com>
2025-08-28 16:43:37 +0600

    removed the misplaced jsonIgnore annotation that was causing the name filed to be ignored.

--- src/main/kotlin/org/ablonewolf/coursecatalogservice/model/dto/request/CourseUpdateDTO.kt
+++ src/main/kotlin/org/ablonewolf/coursecatalogservice/model/dto/request/CourseUpdateDTO.kt
@@ -1,9 +1,6 @@
 package org.ablonewolf.coursecatalogservice.model.dto.request
 
-import com.fasterxml.jackson.annotation.JsonIgnore
-
 data class CourseUpdateDTO(
-	@JsonIgnore
 	val name: String?,
 	val category: String?,
 	val description: String?
