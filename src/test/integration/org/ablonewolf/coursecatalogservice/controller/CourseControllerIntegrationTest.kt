a89980b8299027712cbebc55672a2c3194fe7b03
Arka Bhuiyan <belmont66@duck.com>
2025-08-30 10:36:11 +0600

    created the integration test class for the Course Controller along with the test method for testing the creation of a new course.

---
+++ src/test/integration/org/ablonewolf/coursecatalogservice/controller/CourseControllerIntegrationTest.kt
@@ -0,0 +1,66 @@
+package org.ablonewolf.coursecatalogservice.controller
+
+import org.ablonewolf.coursecatalogservice.model.dto.request.CourseCreateDTO
+import org.ablonewolf.coursecatalogservice.model.dto.response.CourseResponseDTO
+import org.ablonewolf.coursecatalogservice.util.PostgresContainerInitializer
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
+class CourseControllerIntegrationTest : PostgresContainerInitializer() {
+
+	@Autowired
+	lateinit var webTestClient: WebTestClient
+
+	companion object {
+		private lateinit var name: String
+		private lateinit var category: String
+		private lateinit var description: String
+		private lateinit var courseResponseDTO: CourseResponseDTO
+
+		@JvmStatic
+		@BeforeAll
+		fun setup() {
+			name = "Kotlin Programming"
+			category = "Programming"
+			description = "Learn the nuts and bolts of Programming with Kotlin"
+
+			courseResponseDTO = object : CourseResponseDTO {
+				override val id = 1
+				override val name = this@Companion.name
+				override val category = this@Companion.category
+				override val description = this@Companion.description
+			}
+		}
+	}
+
+
+	@Test
+	fun test_courseCreateSuccess_WhenValidCreateDTOProvided_Returns201AndCourseDetails() {
+		// Arrange
+		val courseCreateDTO = CourseCreateDTO(
+			"Kotlin Programming", "Programming",
+			"Learn the nuts and bolts of Programming with Kotlin"
+		)
+
+		// Act & Assert
+		webTestClient.post()
+			.uri("/courses")
+			.bodyValue(courseCreateDTO)
+			.exchange()
+			.expectStatus().isCreated
+			.expectBody()
+			.jsonPath("$.name").isEqualTo("Kotlin Programming")
+			.jsonPath("$.category").isEqualTo("Programming")
+			.jsonPath("$.description").isEqualTo("Learn the nuts and bolts of Programming with Kotlin")
+			.jsonPath("$.id").isEqualTo(1)
+	}
+
+}
