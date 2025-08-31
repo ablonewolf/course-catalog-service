41a05a61ef14076c92cc093a3a36ea33be437bfe
Arka Bhuiyan <belmont66@duck.com>
2025-08-31 16:01:57 +0600

    created the InstructorRepository interface.

---
+++ src/main/kotlin/org/ablonewolf/coursecatalogservice/repository/InstructorRepository.kt
@@ -0,0 +1,9 @@
+package org.ablonewolf.coursecatalogservice.repository
+
+import org.ablonewolf.coursecatalogservice.model.entity.Instructor
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface InstructorRepository : JpaRepository<Instructor, Int> {
+}
