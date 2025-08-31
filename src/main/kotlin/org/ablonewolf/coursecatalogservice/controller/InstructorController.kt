c13c3324662be2e914e7430c743c867b74029fe6
Arka Bhuiyan <belmont66@duck.com>
2025-08-31 16:02:48 +0600

    created the controller class for instructor along with the API for creating a new instructor.

---
+++ src/main/kotlin/org/ablonewolf/coursecatalogservice/controller/InstructorController.kt
@@ -0,0 +1,35 @@
+package org.ablonewolf.coursecatalogservice.controller
+
+import io.swagger.v3.oas.annotations.Operation
+import org.ablonewolf.coursecatalogservice.model.dto.request.InstructorCreateDTO
+import org.ablonewolf.coursecatalogservice.model.dto.request.InstructorResponseDTO
+import org.ablonewolf.coursecatalogservice.service.InstructorService
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/instructors")
+@Validated
+class InstructorController(
+	private val instructorService: InstructorService,
+) {
+	companion object {
+		private val log = LoggerFactory.getLogger(InstructorController::class.java)
+	}
+
+	@PostMapping
+	@Operation(summary = "Add a new instructor")
+	fun createInstructor(@RequestBody instructorCreateDTO: InstructorCreateDTO):
+			ResponseEntity<InstructorResponseDTO> {
+		log.info("Creating new instructor with name ${instructorCreateDTO.name}")
+		val createdInstructor = instructorService.createNewInstructor(instructorCreateDTO)
+		return ResponseEntity.status(HttpStatus.CREATED.value())
+			.body(createdInstructor)
+	}
+}
