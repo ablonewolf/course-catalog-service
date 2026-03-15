a307d3085d47f6906cf2da858ea82efbd7a23e16
Arka Bhuiyan <belmont66@duck.com>
2026-03-15 16:16:55 +0600

    refactor PostgresContainerInitializer to make it abstract and enable container reuse and sharable among multiple test classes.

--- src/test/integration/org/ablonewolf/coursecatalogservice/util/PostgresContainerInitializer.kt
+++ src/test/integration/org/ablonewolf/coursecatalogservice/util/PostgresContainerInitializer.kt
@@ -3,19 +3,20 @@ package org.ablonewolf.coursecatalogservice.util
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.PostgreSQLContainer
-import org.testcontainers.junit.jupiter.Container
-import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.utility.DockerImageName
 
-@Testcontainers
-open class PostgresContainerInitializer {
+abstract class PostgresContainerInitializer {
 
 	companion object {
-		@Container
 		val postgresDB = PostgreSQLContainer(DockerImageName.parse("postgres:16-alpine")).apply {
 			withDatabaseName("course_catalog_test_db")
 			withUsername("postgres")
 			withPassword("secret")
+			withReuse(true)
+		}
+
+		init {
+			postgresDB.start()
 		}
 
 		@JvmStatic
